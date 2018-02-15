bb94d0027387a467020c7ac720ed233431a48394
Xristian93 <cristianfuente72@gmail.com>
2018-02-15 16:35:50 +0100

    Creada clase AgendaTelefonica y metodos set y get

---
+++ AgendaTelefonica.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+/**
+ * Write a description of class AgendaTelefonica here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+    HashMap<String, String> agenda;
+
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        // initialise instance variables
+        agenda = new HashMap<>();
+    }
+
+    /**
+     * Introducir numero de telefono
+     */
+    public void enterNumber (String nombre, String numero){
+        agenda.put(nombre, numero);
+    }
+    
+    /**
+     * Leer numero de telefono
+     */
+    public void lookupNumber (String nombre){
+        System.out.println(agenda.get(nombre));
+    }
+}
